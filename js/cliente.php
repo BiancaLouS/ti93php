4049c75c704876742c9736245ef20856e9cd6800
BiancaLouS <biancalourencosilva@gmail.com>
2022-11-16 16:15:54 -0300

    Atualização cliente.php (Criação de matriz)

--- js/cliente.php
+++ js/cliente.php
@@ -15,3 +15,33 @@
 
     print_r($row);
 ?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clientes (<?php echo $num_rows ?>)</title>
+</head>
+<body>
+        <table>
+            <thead>
+                <th>Cod</th>
+                <th>Nome</th>
+                <th>CPF</th>
+            </thead>
+            <tbody>
+                <?php do {?>
+                    <tr>
+                        <td>
+                            <?php echo $row['cod_cliente'];?></td>
+                            <?php echo $row['nome'];?></td>
+                            <?php echo $row['cpf'];?></td>
+
+
+                    </tr>
+                <?php } while ($row = $lista->fetch())?>
+            </tbody>
+        </table>
+</body>
+</html>
